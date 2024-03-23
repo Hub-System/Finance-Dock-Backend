daf98d5978f685135d184bc3ef598ca669d8d8ed
douglads <douglasfm1995@gmail.com>
2024-03-23 13:30:27 -0300

    first commit

---
+++ src/main/java/com/fincance_dock/finance_dock/FinanceDockApplication.java
@@ -0,0 +1,13 @@
+package com.fincance_dock.finance_dock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceDockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceDockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fincance_dock/finance_dock/FinanceDockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fincance_dock.finance_dock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceDockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
