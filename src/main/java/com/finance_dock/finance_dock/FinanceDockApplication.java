5314c8adb0c334831878debf5b40b930e20502d4
douglads <douglasfm1995@gmail.com>
2024-05-12 22:39:58 -0300

    feat: redirect to front on open root application

--- src/main/java/com/finance_dock/finance_dock/FinanceDockApplication.java
+++ src/main/java/com/finance_dock/finance_dock/FinanceDockApplication.java
@@ -1,9 +1,19 @@
 package com.finance_dock.finance_dock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 @SpringBootApplication
+@Controller
 public class FinanceDockApplication {
+
+	@GetMapping("/")
+    public RedirectView redirectToSpecificUrl() {
+        return new RedirectView("https://finance-dock-frontend.onrender.com");
+    }
+
 	public static void main(String[] args) {
 		SpringApplication.run(FinanceDockApplication.class, args);
 	}
